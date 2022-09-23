262c7cb3b035ab3b114becd1d182381fd02d762a
Markollantes2307 <72058326+Markollantes2307@users.noreply.github.com>
2022-09-23 14:54:05 -0500

    Registrar mascotas en veterinaria actualizado

--- src/healthypets/pages/pet-register.component.vue
+++ src/healthypets/pages/pet-register.component.vue
@@ -1,24 +1,34 @@
 <template>
-  <pv-sidebar>
-    <h2 font-size="35px" font-weight="bold">Healthy Pets</h2>
-  </pv-sidebar>
-  <div class="flex align-content-center">
-    <div class="align-content-center">
-      <img src="Veterinaria1.png" width="355px" height="355px" border-radius="50%">
-    </div>
-    <div class="justify-content-center">
-      <h3 font-size="24px" font-weight="bold">Veterinaria Lima</h3>
-      <p font-size="15px">
-        Fundada en 2008, se especializa principalmente en atender emergencias veterinarias y también ofrece servicios cosmeticos
-      </p>
-      <h3 font-size="24px" font-weight="bold">Comentarios destacados:</h3>
-      <ul list-style-type="disc">
-        <li font-size="15px" font-style="italic">"De los mejores servicios que he recibido"</li>
-        <li font-size="15px" font-style="italic">"Lastima que no mucha gente conoce este lugar, se merece más clientes"</li>
-        <li font-size="15px" font-style="italic">"Una buena alternativa con buenos precios"</li>
-      </ul>
-      <pv-button label="Registrar Mascota" class="p-button-success"/>
-    </div>
+  <div>
+    <pv-sidebar v-model:visible="visibleLeft">
+      <h2>Healthy Pets</h2>
+    </pv-sidebar>
+  </div>
+  <div>
+    <pv-card>
+      <template #content>
+        <img src="Veterinaria1.png" width="355px" height="355px" border-radius="50%" alt="logoVetLima">
+      </template>
+    </pv-card>
+    <pv-card>
+      <template #title>
+        {{ ('veterinaries.name') }}
+      </template>
+      <template #content>
+        {{ ('veterinaries.content') }}
+      </template>
+      <template #title>
+        Comentarios destacados:
+      </template>
+      <template #content>
+        <ul>
+          <li>{{ ('commentaries.comment') }}</li>
+        </ul>
+      </template>
+      <template #footer>
+        <p-button label="Registra mascota aquí" icon="pi pi-check"/>
+      </template>
+    </pv-card>
   </div>
 </template>
 
@@ -27,7 +37,7 @@ export default {
   name: "vetinfo"
 }
 </script>
-
+    
 <style scoped>
-
+    
 </style>
