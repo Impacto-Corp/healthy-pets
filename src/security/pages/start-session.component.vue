4cfd71677e09af0abf685748349e0321cf86517f
x5Anthony <66744988+x5Anthony@users.noreply.github.com>
2022-09-28 12:12:06 -0500

    fead: Start-session implemented

--- src/security/pages/start-session.component.vue
+++ src/security/pages/start-session.component.vue
@@ -1,73 +1,114 @@
 <template>
+  <div class="start-session">
+    <div class="title">
+      <p>Healthy Pets</p>
+      <br>
+      <p>The platform to find your ideal veterinarian</p>
+      <img src="src/assets/Logotipo.jpg">
+    </div>
 
-  <div class="title">
-    <p>Healthy Pets</p>
-    <br>
-    <p>The platform to find your ideal veterinarian</p>
-    <img src="../../assets/Logotipo.JPG">
+    <div class="card">
+      <h1 class="mt-7">Sign in to Healthy Pets</h1>
+      <hr>
+      <h4 class="mt-7 ml-8"> Username or Email Address</h4>
+      <span class="p-float-label ml-8">
+         <pv-input-text type="text" v-model="email1" class="border-3 ml-12"/>
+      </span>
+
+      <h4 class="mt-7 ml-8">Password</h4>
+      <span class="p-float-label ml-8">
+         <pv-input-text type="text" v-model="password1" class="border-3 ml-12"/>
+      </span>
+      <div class="b-start m-6" >
+          <pv-button label="Log in"
+                   class="p-button-rounded "
+                   @click="validation"
+        />
+          <pv-button label="Create account"
+                   class="p-button-rounded "
+        />
+    </div>
   </div>
-
-  <div class="card">
-    <h1>Sign in to Healthy Pets</h1>
-    <br>
-    <hr>
-    <br>
-    <h4>Username or Email Address</h4>
-    <pv-input-text style="padding: 10px 300px; text-align: left" type="text" v-model="text"/>
-    <br>
-    <h4>Password</h4>
-    <pv-input-text v-model="password"/>
-    <br>
-    <br>
-    <span>
-    <button class="mr-3" style="padding: 5px 50px;background-color: #ffa448;border-radius: 20px"  >Log in</button>
-      <button style="padding: 5px 23px;background-color: #ffa448;border-radius: 20px" >Create account</button>
-
-    </span>
   </div>
-
 </template>
 
 <script>
-
-
-
 export default {
-  name: "start-session",
+  name: "start-session.component",
   data() {
     return {
-      text: '',
-      password:'',
+      email1: 'xxx',
+      password1:'123',
+
+    }
+  },
+  Users: [
+    {
+      email2:'yo@out.com',
+      password2:'12345'
+    },
+    {
+      email2:'tu@gma.com',
+      password2:'67890'
+    }
+  ],
+
+  methods:{
+
+    validation() {
+        const res=this.Users.some(User=>User.email2===this.email1)
+          if(res){
+            console.log('funca')
+          }else {
+            console.log('no funca')
+          }
     }
   }
 }
+
 </script>
 
 <style>
-{
-  background:url("../../assets/Fondo1.png");
-}
-img{
+  .start-session{
+    width: 100%;
+    height: 850px;
+    background: url("./src/assets/Fondo1.png");
+    display: flex;
+    justify-content: center;
+  }
+
+  img{
   width: 400px;
   display:flex;
-}
-.title{
-
-  text-align: justify;
-  font-size: 35px;
-  font-weight: bold;
-  color: #667080;
-  left: -50px;
-  top: -100px;
-  font-family: Roboto;
+  }
+  .title{
+    float:left;
+    width: 50%;
+    align-content: center;
+    font-size: 30px;
+    font-weight: bold;
+    left:20px;
+    top:80px;
+    position: relative;
+    color: #667080;
+    font-family: Roboto;
 }
 .card {
+  float:left;
+  width: 50%;
   color: #667080;
   display: flow;
+  left:-50px;
+  top:80px;
+  position: relative;
   font-family: Roboto;
 }
-Button{
+  {
+    padding: 10px 300px;
+  }
+.b-start{
   color: white;
-  font-family: Roboto;
+  font-family:Roboto;
+
 }
 </style>
