0deeb8fceb816f4231461f2e680422979da63e4d
Bruno Palomino <89095679+bruno1924@users.noreply.github.com>
2022-09-28 12:26:35 -0500

    feat:Home-view implemented

---
+++ src/healthypets/pages/home-view.component.vue
@@ -0,0 +1,10 @@
+<template>
+
+</template>
+<script>
+import ToolbarComponent from "@/healthypets/pages/toolbar.component.vue";
+export default {
+  name: "home-view",
+  components:{ToolbarComponent},
+}
+</script>
